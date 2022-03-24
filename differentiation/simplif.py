9410f091c49325facec63a9238393348a18b70cf
Даниил Смирнов <smirnov.daniil@phystech.edu>
2022-03-24 20:37:54 +0300

    "power" brench

--- differentiation/simplif.py
+++ differentiation/simplif.py
@@ -10,6 +10,7 @@ def simplificator(equation):
                 if equation[i+2].isdigit() or equation[i+2] == '.':
                     exceptions.append(i)
         equation = helpFunc.replacer(equation, '^1', '', exceptions)
+
     parts = helpFunc.founder(equation, '^0')
     exceptions = []
     if len(parts) != 0:
@@ -17,8 +18,11 @@ def simplificator(equation):
             if i+2 < len(equation):
                 if equation[i+2] == '.':
                     exceptions.append(i)
+        for i in range(len(exceptions)):
+            exceptions[i] -= 1
         equation = helpFunc.replacer(equation, 'x^0', '1', exceptions)
-    parts = helpFunc.founder(equation, '1*')
+
+    parts = helpFunc.founder(equation, '1*') 
     exceptions = []
     if len(parts) > 0:
         for i in parts:
@@ -26,6 +30,7 @@ def simplificator(equation):
                 if equation[i-1].isdigit():
                     exceptions.append(i)
         equation = helpFunc.replacer(equation, '1*', '', exceptions)
+
     parts = helpFunc.founder(equation, '*1')
     exceptions = []
     if len(parts) > 0:
@@ -35,4 +40,6 @@ def simplificator(equation):
                     exceptions.append(i)
         equation = helpFunc.replacer(equation, '*1', '', exceptions)
 
+    equation = helpFunc.replacer(equation, '*(1)', '', exceptions = [])
+    equation = helpFunc.replacer(equation, '(1)*', '', exceptions = [])
     return equation
