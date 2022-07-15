b9f59fc274325cf19fb6000e1991625710f25d06
Matanalysis <smirnov.daniil@phystech.edu>
2022-07-15 23:39:11 +0300

    hotfix for fractional number in power and log functions

--- differentiation/simpleDeriv.py
+++ differentiation/simpleDeriv.py
@@ -1,4 +1,5 @@
 import diff
+import helpFunc
 
 def simplePow(function, n):     #n - number, not function
     if '.' in n:
@@ -9,21 +10,27 @@ def simplePow(function, n):     #n - number, not function
 
 def diffPower(function1, function2):
     if function1 == 'e':                
-        if function2.isdigit():         #e.g e^2
+        if helpFunc.isNumber(function2):         #e.g e^2
             return '0'  
         else:                           #e.g e^sin
             return diff.distr(function2) + '*power(e,' + function2 + ')'
-    elif function1.isdigit():
-        if function2.isdigit():         #e.g 2^2
+    elif helpFunc.isNumber(function1):
+        if helpFunc.isNumber(function2):         #e.g 2^2
             return '0'
         else:                           #e.g 2^sin
             return 'log(e,' + function1 + ')*' + diff.distr(function2) + '*power(' + function1 + ',' + function2 + ')'
     else:
-        if function2.isdigit() or function2[0] == '-' and function2[1:].isdigit():          
-            if int(function2) >= 0:      #e.g sin^2
-                return function2 + '*' + 'power(' + function1 + ',' + str(int(function2) - 1) + ')*(' + diff.distr(function1) + ')'
-            else:                       #e.g sin^(-2)
-                return '(' + function2 + ')*' + 'power(' + function1 + ',' + str(int(function2) - 1) + ')*(' + diff.distr(function1) + ')'
+        if helpFunc.isNumber(function2):          
+            if float(function2) >= 0:      #e.g sin^2 or sin^0.5
+                if float(function2)%1 != 0:    #sin^0.5
+                    return function2 + '*' + 'power(' + function1 + ',' + str(float(function2) - 1) + ')*(' + diff.distr(function1) + ')'
+                else:                           #sin^2
+                    return function2 + '*' + 'power(' + function1 + ',' + str(int(function2) - 1) + ')*(' + diff.distr(function1) + ')'
+            else:                       #e.g sin^(-2) or sin^(-0.5)
+                if float(function2)%1 != 0:    #sin^(-0.5)
+                    return '(' + function2 + ')*' + 'power(' + function1 + ',' + str(float(function2) - 1) + ')*(' + diff.distr(function1) + ')'
+                else:                           #sin^(-2)
+                    return '(' + function2 + ')*' + 'power(' + function1 + ',' + str(int(function2) - 1) + ')*(' + diff.distr(function1) + ')'
         else:                           #e.g sin^cos
             return '(' + diff.distr(function1) + '*' + function2 + '*power(' + function1 + ',-1)' + '+' + 'log(e,' + function1 + ')*' + diff.distr(function2) + ')*power(' + function1 + ',' + function2 + ')'
 
@@ -40,12 +47,12 @@ def diffTrig(function):     #just simple derivates of triginometric functions
 
 def diffLog(function1, function2):
     if function1 == 'e':                
-        if function2.isdigit():         # e.g log(e, 2)
+        if helpFunc.isNumber(function2):         # e.g log(e, 2)
             return '0'  
         else:                           # e.g log(e, sin)
             return 'power(' + function2  + ',-1)*(' + diff.distr(function2) + ')'
-    elif function1.isdigit():
-        if function2.isdigit():         # e.g log(2, 2)
+    elif helpFunc.isNumber(function1):
+        if helpFunc.isNumber(function2):         # e.g log(2, 2)
             return '0'
         else:  
             return 'power(log(e,' + function1 + '),-1)*' + 'power(' + function2  + ',-1)*(' + diff.distr(function2) + ')'
